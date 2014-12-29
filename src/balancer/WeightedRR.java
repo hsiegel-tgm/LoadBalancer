6635ddb6157bab63afbbfd383776d14340c87e92
hsiegel-tgm <hannah.siegel@student.tgm.ac.at>
2014-12-29 20:12:42 +0100

    wrr works

--- src/balancer/WeightedRR.java
+++ src/balancer/WeightedRR.java
@@ -8,6 +8,7 @@ import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.HashMap;
 import java.util.Map.Entry;
+import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -18,9 +19,10 @@ import server.ServerCalculator;
 
 public class WeightedRR implements Balancer {
 	private HashMap<String, ServerCalculator> m_servers = new HashMap<String,ServerCalculator>();
-
+	private String server_weighted[];
+	private int m_iterator;
+	
 	public WeightedRR(String name){
-		
 		Balancer x;
 		//Exporting stub
 		try {
@@ -38,10 +40,9 @@ public class WeightedRR implements Balancer {
 			e.printStackTrace();
 		}
 		Log.info("Started Weighted Round Robin LB ... ");
-
 	}
 	
-	public BigDecimal pi() {
+	/* public BigDecimal pi() {
 		Log.debug("LB got the request");
 		if(m_servers.size()>0){
 			for (Entry entry : m_servers.entrySet()) {
@@ -61,11 +62,130 @@ public class WeightedRR implements Balancer {
 			Log.info("Sorry. But there is no server availiable.");
 		}
 		return null;
-	}
+	} */
+	
+	/* private void allocate(){
+		HashMap<String, Integer> availiable_servers = new HashMap<String,Integer>();
 
+		for (Entry entry : m_servers.entrySet()) {
+			 ServerCalculator obj = (ServerCalculator) entry.getValue();
+			 try {
+				 availiable_servers.put(entry.getKey().toString(),  obj.getWeight()); 
+			} catch (RemoteException e) {
+				Log.error("There was an remote exception when communication with one of the servers");
+			}
+		}
+		
+		int number = 0;
+		for (Entry entry : availiable_servers.entrySet()) {
+			Integer obj = (Integer) entry.getValue();
+			number += obj;
+		}
+		
+		Log.debug("Allocation has found out a total amount of "+ number +" units");
+		
+		int servers[] = new int[number];
+		
+		int already_allocated = 0;
+		
+		for(int j = 0 ; j < number ; ++j ){
+			if()
+			servers[j]
+		}
+		
+	} */
+	
+	
+	private void allocate(){
+		HashMap<String, Integer> availiable_servers = new HashMap<String,Integer>();
+
+		for (Entry entry : m_servers.entrySet()) {
+			 ServerCalculator obj = (ServerCalculator) entry.getValue();
+			 try {
+				 availiable_servers.put(entry.getKey().toString(),  obj.getWeight()); 
+			} catch (RemoteException e) {
+				Log.error("There was an remote exception when communication with one of the servers");
+			}
+		}
+		
+		int number = 0;
+		for (Entry entry : availiable_servers.entrySet()) {
+			Integer obj = (Integer) entry.getValue();
+			number += obj;
+		}
+		
+		Log.debug("Allocation has found out a total amount of "+ number +" units");
+		
+		String servers[] = new String[number];
+		
+		int already_allocated = 0;
+		
+		while (already_allocated < number ){
+			for (Entry entry : availiable_servers.entrySet()) {
+				Integer capcity = (Integer) entry.getValue();
+				if(capcity>0){
+					String key = entry.getKey().toString();
+					servers[already_allocated] = key;
+					already_allocated++;
+				}
+			}
+			
+			HashMap<String, Integer> availiable_servers2 = new HashMap<String,Integer>();
+
+			for (Entry entry : availiable_servers.entrySet()) {
+				Integer capcity = (Integer) entry.getValue();
+				availiable_servers2.put(entry.getKey().toString(),capcity-1);
+			}
+			availiable_servers = availiable_servers2;
+		}
+		
+		for(int i =0 ; i< number; i++){
+			Log.debug(servers[i]);
+		}
+		availiable_servers.clear();
+		
+		while(m_iterator != 0) {}
+		
+		server_weighted = servers;
+	}
+	
+	
+	private int getNumbers(){
+		int num = 0;
+		for (Entry entry : m_servers.entrySet()) {
+			 ServerCalculator obj = (ServerCalculator) entry.getValue();
+			 try {
+				num += obj.getWeight();
+			} catch (RemoteException e) {
+				Log.error("There was an remote exception when communication with one of the servers");
+			}
+		}
+		return num;
+	}
+	
+	public BigDecimal pi() {
+		Log.debug("LB got the request ... ");
+		
+		ServerCalculator server_choosen = (ServerCalculator) m_servers.get(server_weighted[m_iterator]);
+		m_iterator++;
+		
+		if(m_iterator==server_weighted.length){
+			m_iterator = 0;
+		}
+		
+		try {
+			return server_choosen.pi();
+		} catch (RemoteException e) {
+			Log.error("There was a remote Exception while communicating with the Server");
+			return new BigDecimal(3.14);
+		}
+	}
+	
 	public void register(ServerCalculator s,String name) throws RemoteException {
 		m_servers.put(name,s);
 		Log.info("Server " +name+ " registered at LoadBalancer. Current number of servers: "+m_servers.size() );
+		Log.debug("Calling allocation...");
+		allocate();
 	}
 
 	public void unregister(ServerCalculator s,String name) throws RemoteException {
--- src/main/Starter.java
+++ src/main/Starter.java
@@ -13,14 +13,22 @@ public class Starter {
 		System.setProperty("java.security.policy", policy.toString());
 
 		if( checkArguments(arg) ) {
-			new WeightedRR("wrr-loadbalancingserver");
-			
-			//new Server("127.0.0.1","wrr-loadbalancingserver",10,"Server1");
-			//new Client("127.0.0.1","wrr-loadbalancingserver",5000);
-			
-			new SimulateServers("127.0.0.1","wrr-loadbalancingserver",10);
-			new SimulateClients("127.0.0.1","wrr-loadbalancingserver",20);
+			if(arg[1].equals("wrr")){
+				new WeightedRR("wrr-loadbalancingserver");
+				new SimulateServers("127.0.0.1","wrr-loadbalancingserver",10);
+				new SimulateClients("127.0.0.1","wrr-loadbalancingserver",20);
 
+			}
+			else{
+				new AgendBasedAdaptive("aba-loadbalancingserver");
+				new SimulateServers("127.0.0.1","aba-loadbalancingserver",10);
+				new SimulateClients("127.0.0.1","aba-loadbalancingserver",20);
+			}
+			
+			//new Server("127.0.0.1","wrr-loadbalancingserver",2,"Server1");
+			//new Server("127.0.0.1","wrr-loadbalancingserver",3,"Server2");
+			//new Client("127.0.0.1","wrr-loadbalancingserver",5000,"Client1");
+			//new Client("127.0.0.1","wrr-loadbalancingserver",2500,"Client2");
 		}
 		else{
 			System.out.println("Please call the program with some program parameters: \n -m method{wrr|aba}  ");
